ffd1c7228b35f3b27ed6262f29bf3f047db74bdd
gregor <itvgo@itv.uni-stuttgart.de>
2018-07-26 10:59:36 +0200

    add MIT License

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ config = {
     'author'                 : 'Gregor Olenik',
     'author_email'           : 'go@hpsim.de',
     'description'            : '',
-    'license'                : 'BSD',
+    'license'                : 'MIT',
     'version'                : "0.1",
     'packages'               : ["FoamMon"],
     'install_requires'       : [
