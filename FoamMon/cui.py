d2c3110553f7bfe988e2cddaa4941e4faab28a17
gregor <itvgo@itv.uni-stuttgart.de>
2018-09-04 14:20:15 +0200

    add version string

--- FoamMon/cui.py
+++ FoamMon/cui.py
@@ -1,4 +1,6 @@
 import urwid
+from .version import __version__ as version
+
 foamMonHeader = """
 8 8888888888       ,o888888o.           .8.                   ,8.       ,8.                   ,8.       ,8.           ,o888888o.     b.             8
 8 8888          . 8888     `88.        .888.                 ,888.     ,888.                 ,888.     ,888.       . 8888     `88.   888o.          8
@@ -10,8 +12,8 @@ foamMonHeader = """
 8 8888         `8 8888       ,8P .8'   `8. `88888.     ,8'     `8.`'     `8.`8888.     ,8'     `8.`'     `8.`8888.`8 8888       ,8P  8      `Y8o. `Y8
 8 8888          ` 8888     ,88' .888888888. `88888.   ,8'       `8        `8.`8888.   ,8'       `8        `8.`8888.` 8888     ,88'   8         `Y8o.`
 8 8888             `8888888P'  .8'       `8. `88888. ,8'         `         `8.`8888. ,8'         `         `8.`8888.  `8888888P'     8            `Yo
-                                                                                                              by Gregor Olenik, go@hpsim.de, hpsim.de
-"""
+                                                                                          by Gregor Olenik, go@hpsim.de, hpsim.de, Version: {}
+""".format(version)
 
 import os
 from .FoamDataStructures import Cases
